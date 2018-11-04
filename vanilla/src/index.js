3c6695f18477493e76e8c895e67c1705d83bfff6
Ken Wu <ken.wu@ibm.com>
2018-11-04 13:03:39 -0500

    first

---
+++ vanilla/src/index.js
@@ -0,0 +1,7 @@
+(function () {
+    var counterEl = document.getElementById('counter');
+    var incrementEl = document.getElementById('increment');
+    incrementEl.addEventListener('click', function() {
+      counterEl.innerHTML = (parseInt(counterEl.innerHTML, 10) + 1).toString();
+    });
+  })();
